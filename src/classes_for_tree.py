f4f29235dae03516a2e5eb5560d3c877ab52f5a6
marianelia <m.lagutkina2014@yandex.ru>
2023-02-14 17:46:39 +0300

    названия методов классов приведены под стандарт

--- src/classes_for_tree.py
+++ src/classes_for_tree.py
@@ -15,19 +15,19 @@ class DataFromParam:
         self.__name :string
         self.__type :string
 
-    def setName(self, name:string) -> None:
+    def set_name(self, name:string) -> None:
         self.__name = name
     
-    def setType(self, type:string) -> None:
+    def set_type(self, type:string) -> None:
         self.__type = type
 
-    def getName(self) -> string:
+    def get_name(self) -> string:
         return self.__name
     
-    def getType(self) -> string:
+    def get_type(self) -> string:
         return self.__type
 
-    def printForTests(self)-> None:
+    def print_for_tests(self)-> None:
         print(self.__name)
         print(self.__type)
 
@@ -36,15 +36,15 @@ class DataFromVariable:
         self.__access :Access = access
         self.__variable :DataFromParam = param
 
-    def getAccess(self) -> Access:
+    def get_access(self) -> Access:
         return self.__access
 
-    def getVariable(self) -> DataFromParam:
+    def get_variable(self) -> DataFromParam:
         return self.__variable
 
-    def printForTests(self)-> None:
+    def print_for_tests(self)-> None:
         print(self.__access)
-        self.__variable.printForTests()
+        self.__variable.print_for_tests()
 
 class DataFromFunc:
     def __init__(self) -> None:
@@ -53,45 +53,45 @@ class DataFromFunc:
         self.__output_param :string
         self.__list_input_params :list[DataFromParam] = []
 
-    def setNamespace(self, ns:string) -> None:
+    def set_namespace(self, ns:string) -> None:
         self.__namespaces.append(ns)
 
-    def setName(self, name:string) -> None:
+    def set_name(self, name:string) -> None:
         self.__name = name
 
-    def setOutParam(self, out_param:string) -> None:
+    def set_out_param(self, out_param:string) -> None:
         self.__output_param = out_param
 
-    def setOutParamFromDecl(self, out_param:string) -> None:
+    def set_out_param_from_decl(self, out_param:string) -> None:
         self.__output_param = out_param.partition(' (')[0]
 
-    def setInpParam(self, inp_type:string, inp_name:string) -> None:
+    def set_inp_param(self, inp_type:string, inp_name:string) -> None:
         input_params :DataFromParam = DataFromParam()
-        input_params.setType(inp_type)
-        input_params.setName(inp_name)
+        input_params.set_type(inp_type)
+        input_params.set_name(inp_name)
         self.__list_input_params.append(input_params)
 
-    def getNamespaces(self) -> list:
+    def get_namespaces(self) -> list:
         return self.__namespaces
 
-    def getName(self) -> string:
+    def get_name(self) -> string:
         return self.__name
 
-    def getOutParam(self) -> string:
+    def get_out_param(self) -> string:
         return self.__output_param
 
-    def getInpParams(self) -> list:
+    def get_inp_params(self) -> list:
         return self.__list_input_params
 
-    def getInpParam(self, index:int) -> DataFromParam:
+    def get_inp_param(self, index:int) -> DataFromParam:
         return self.__list_input_params[index]
 
-    def printForTests(self) -> None:
+    def print_for_tests(self) -> None:
         print(self.__namespaces)
         print(self.__name)
         print(self.__output_param)
         for num_inp_param in range(len(self.__list_input_params)):
-            self.__list_input_params[num_inp_param].printForTests()
+            self.__list_input_params[num_inp_param].print_for_tests()
 
 
 class DataFromMethod:
@@ -99,15 +99,15 @@ class DataFromMethod:
         self.__access :Access = access
         self.__function :DataFromFunc = func
 
-    def getAccess(self) -> Access:
+    def get_access(self) -> Access:
         return self.__access
 
-    def getFunction(self) -> DataFromFunc:
+    def get_function(self) -> DataFromFunc:
         return self.__function
         
-    def printForTests(self)-> None:
+    def print_for_tests(self)-> None:
         print(self.__access)
-        self.__function.printForTests()
+        self.__function.print_for_tests()
 
 
 class DataFromStruct:
@@ -118,51 +118,51 @@ class DataFromStruct:
         self.__list_methods :list[DataFromMethod] = []
         self.__list_variable :list[DataFromVariable] = []
 
-    def setAccess(self,access:Access) -> None:
+    def set_access(self,access:Access) -> None:
         self.__default_access = access
 
-    def getAccess(self) -> Access:
+    def get_access(self) -> Access:
         return self.__default_access
     
-    def setNamespace(self, ns:string) -> None:
+    def set_namespace(self, ns:string) -> None:
         self.__namespaces.append(ns)
 
-    def setName(self, name:string) -> None:
+    def set_name(self, name:string) -> None:
         self.__name = name
 
-    def getName(self) -> string:
+    def get_name(self) -> string:
         return self.__name
     
-    def getNamespaces(self) -> list:
+    def get_namespaces(self) -> list:
         return self.__namespaces
 
-    def setMethod(self, access:string, func:DataFromFunc) -> None:
+    def set_method(self, access:string, func:DataFromFunc) -> None:
         method :DataFromMethod = DataFromMethod(access, func)
         self.__list_methods.append(method)
 
-    def getMethods(self) -> list:
+    def get_methods(self) -> list:
         return self.__list_methods
 
-    def getMethodByIndex(self, index:int) -> DataFromMethod:
+    def get_method_by_index(self, index:int) -> DataFromMethod:
         return self.__list_methods[index]
 
 
-    def setVariable(self, access:string, var:DataFromParam) -> None:
+    def set_variable(self, access:string, var:DataFromParam) -> None:
         variable :DataFromVariable = DataFromVariable(access, var)
         self.__list_variable.append(variable)
 
-    def getVariable(self) -> list:
+    def get_variable(self) -> list:
         return self.__list_variable
 
-    def getVariableByIndex(self, index:int) -> DataFromVariable:
+    def get_variable_by_index(self, index:int) -> DataFromVariable:
         return self.__list_variable[index]
 
-    def printForTests(self)-> None:
+    def print_for_tests(self)-> None:
         print(self.__namespaces)
         print(self.__name)
         
         for num_method in range(len(self.__list_methods)):
-            self.__list_methods[num_method].printForTests()
+            self.__list_methods[num_method].print_for_tests()
 
         for num_var in range(len(self.__list_variable)):
-            self.__list_variable[num_var].printForTests()
+            self.__list_variable[num_var].print_for_tests()
--- src/data.py
+++ src/data.py
@@ -8,89 +8,74 @@ class Data:
         self.__list_data_struct :list[DataFromStruct] = []
         #...
 
-    def addDataFromFunc(self, data:DataFromFunc):
+    def add_data_from_func(self, data:DataFromFunc):
         self.__list_data_func.append(data)
     
-    def addDataFromStruct(self, data:DataFromStruct):
+    def add_data_from_struct(self, data:DataFromStruct):
         self.__list_data_struct.append(data)
 
-    def serializeData(self):
+    def serialize_data(self):
         file_obj = code_data_pb2.File()
         #file.file_name = ..  
         for num_data_func in range(len(self.__list_data_func)):
             func_obj = file_obj.function_list.add()
-            self.serializeFunc(func_obj, self.__list_data_func[num_data_func])
+            self.serialize_func(func_obj, self.__list_data_func[num_data_func])
 
         for num_data_struct in range(len(self.__list_data_struct)):
             struct_obj = file_obj.struct_list.add()
-            self.serializeStruct(struct_obj, self.__list_data_struct[num_data_struct])
+            self.serialize_struct(struct_obj, self.__list_data_struct[num_data_struct])
 
-        serializeToString = file_obj.SerializeToString()
-        self.serializeDataToFile("test", serializeToString)
-        print(serializeToString)
-        file_obj.ParseFromString(serializeToString)
+        serialize_to_string = file_obj.SerializeToString()
+        self.serialize_data_to_file("test", serialize_to_string)
+        print(serialize_to_string)
+        file_obj.ParseFromString(serialize_to_string)
         print(file_obj)
 
-    def serializeDataToFile(self, file_name:string, data:string):
+    def serialize_data_to_file(self, file_name:string, data:string):
         file = open(file_name, 'wb')
         file.write(data)
         file.close()
 
-    def serializeStruct(self, struct_proto_format, struct:DataFromStruct):
-        struct_proto_format.namespace.extend(struct.getNamespaces())
-        struct_proto_format.name = struct.getName()
+    def serialize_struct(self, struct_proto_format, struct:DataFromStruct):
+        struct_proto_format.namespace.extend(struct.get_namespaces())
+        struct_proto_format.name = struct.get_name()
 
-        for num_method in range(len(struct.getMethods())):
+        for num_method in range(len(struct.get_methods())):
             method_obj = struct_proto_format.methods.add()
-            self.serializeMethod(method_obj, struct.getMethodByIndex(num_method))
+            self.serialize_method(method_obj, struct.get_method_by_index(num_method))
 
-        for num_var in range(len(struct.getVariable())):
+        for num_var in range(len(struct.get_variable())):
             var_obj = struct_proto_format.variables.add()
-            self.serializeVariable(var_obj, struct.getVariableByIndex(num_var))
+            self.serialize_variable(var_obj, struct.get_variable_by_index(num_var))
 
 
-    def enumAccessToProtobuf(self, access:Access):
-        #print("serialize ACCESS " + str(access))
+    def enum_access_to_protobuf(self, access:Access):
         if (access == Access.PUBLIC):
-            #print("serialize ACCESS " + str(code_data_pb2.PUBLIC))
             return code_data_pb2.PUBLIC
+
         elif (access == Access.PRIVATE):
-            #print("serialize ACCESS " + str(code_data_pb2.PRIVATE))
             return code_data_pb2.PRIVATE
+
         elif (access == Access.PROTECTED):
-            #print("serialize ACCESS " + str(code_data_pb2.PROTECTED))
             return code_data_pb2.PROTECTED
         return None
 
-    def serializeVariable(self, var_proto_format, var:DataFromVariable):
-        var_proto_format.access = self.enumAccessToProtobuf(var.getAccess())
-        self.serializeInputParams(var_proto_format.variable, var.getVariable())
+    def serialize_variable(self, var_proto_format, var:DataFromVariable):
+        var_proto_format.access = self.enum_access_to_protobuf(var.get_access())
+        self.serialize_input_params(var_proto_format.variable, var.get_variable())
     
+    def serialize_method(self, method_proto_format, method:DataFromMethod):
+        method_proto_format.access = self.enum_access_to_protobuf(method.get_access())
+        self.serialize_func(method_proto_format.function, method.get_function())        
 
-    def serializeMethod(self, method_proto_format, method:DataFromMethod):
-        #method_proto_format.access = self.enumAccessToProtobuf(method.getAccess())
-
-        if (method.getAccess() == Access.PUBLIC):
-            #print("serialize ACCESS " + str(code_data_pb2.PUBLIC))
-            method_proto_format.access = code_data_pb2.PUBLIC
-        elif (method.getAccess() == Access.PRIVATE):
-            #print("serialize ACCESS " + str(code_data_pb2.PRIVATE))
-            method_proto_format.access = code_data_pb2.PRIVATE
-        elif (method.getAccess() == Access.PROTECTED):
-            #print("serialize ACCESS " + str(code_data_pb2.PROTECTED))
-            method_proto_format.access = code_data_pb2.PROTECTED
-        
-        print("serialize ACCESS " + str(method_proto_format.access))
-        self.serializeFunc(method_proto_format.function, method.getFunction())        
-
-    def serializeFunc(self, func_proto_format, func:DataFromFunc):
-        func_proto_format.namespace.extend(func.getNamespaces())
-        func_proto_format.name = func.getName()
-        func_proto_format.output_param = func.getOutParam()
-        for num_inp_param in range(len(func.getInpParams())):
+    def serialize_func(self, func_proto_format, func:DataFromFunc):
+        func_proto_format.namespace.extend(func.get_namespaces())
+        func_proto_format.name = func.get_name()
+        func_proto_format.output_param = func.get_out_param()
+        for num_inp_param in range(len(func.get_inp_params())):
             inp_param_obj = func_proto_format.input_params.add()
-            self.serializeInputParams(inp_param_obj, func.getInpParam(num_inp_param))
+            self.serialize_input_params(inp_param_obj, func.get_inp_param(num_inp_param))
 
-    def serializeInputParams(self, param_proto_format, inp_param:DataFromParam):
-        param_proto_format.name = inp_param.getName()
-        param_proto_format.type = inp_param.getType()
+    def serialize_input_params(self, param_proto_format, inp_param:DataFromParam):
+        param_proto_format.name = inp_param.get_name()
+        param_proto_format.type = inp_param.get_type()
