b1d8f5a120fe0b81bac4562f013071a6e30d0758
marianelia <m.lagutkina2014@yandex.ru>
2023-03-12 21:23:47 +0300

    добавлен обход файлов проекта

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,27 @@
 import sys
 import pathlib
+import os
 sys.path.append(str(pathlib.Path(sys.path[0]).resolve().parent / "src"))
 
 from parser_tree import Parser
 
-root = str(pathlib.Path(sys.path[0]).resolve())
-fn1 = root + '/test1.cpp'
-fn2 = root +'/test2.cpp'
-file_names = [
-    #fn1,
-    fn2
-    ]
+# root = str(pathlib.Path(sys.path[0]).resolve())
+# fn1 = root + '/test1.cpp'
+# fn2 = root +'/test2.cpp'
+# file_names = [
+#     #fn1,
+#     fn2
+#     ]
 
+path_to_project = str(pathlib.Path(sys.path[0]).resolve())
 
 if __name__ == '__main__':
-    for fn in file_names:
-        print(fn)
-        parser_tree = Parser()
-        parser_tree.parser_tree_from_file(fn, ['-std=c++17'])
-        parser_tree.serialize_data_to_binary_file("/home/maria/diploma/generator/tests/")
-        del parser_tree
+    for root, dirs, files in os.walk(path_to_project):
+        for name in files:
+            if(name.endswith(".cpp")):
+                # print(os.path.join(root, name))
+                parser_tree = Parser()
+                parser_tree.parser_tree_from_file(os.path.join(root, name), ['-std=c++17'])
+                parser_tree.serialize_data_to_binary_file("/home/maria/diploma/generator/tests/")
+                del parser_tree
+                
