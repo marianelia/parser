5ef2a62988026732b4e3562c17362a6f06b479d6
marianelia <m.lagutkina2014@yandex.ru>
2023-03-28 14:41:15 +0300

    исправлен баг: вызов сериализации длякаждого файла отдельно

--- tests/test.py
+++ tests/test.py
@@ -16,11 +16,12 @@ from parser_tree import Parser
 path_to_project = str(pathlib.Path(sys.path[0]).resolve())
 
 if __name__ == '__main__':
+    parser_tree = Parser()
     for root, dirs, files in os.walk(path_to_project):
         for name in files:
             if(name.endswith(".cpp")):
-                parser_tree = Parser()
                 parser_tree.parser_tree_from_file(os.path.join(root, name), ['-std=c++17'])
-                parser_tree.serialize_data_to_binary_file("/home/maria/diploma/generator/tests/")
-                del parser_tree
+
+    parser_tree.serialize_data_to_binary_file("/home/maria/diploma/generator/tests/")
+    del parser_tree
                 
