a1f721e786d0329450ca74a6d90a493e956059ea
marianelia <m.lagutkina2014@yandex.ru>
2023-05-17 00:22:08 +0300

    дописаны пути до проекта с библиотекой, подаваемой как аргумент

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ sys.path.append(str(pathlib.Path(sys.path[0]).resolve() / "src"))
 
 from parser_tree import Parser
 
-# root = str(pathlib.Path(sys.path[0]).resolve())
-# fn1 = root + '/test1.cpp'
-# fn2 = root +'/test2.cpp'
-# file_names = [
-#     #fn1,
-#     fn2
-#     ]
-
-#path_to_project = str(pathlib.Path(sys.path[0]).resolve().parent.parent / 'test_lib')
-#path_to_project = str(pathlib.Path(sys.path[0]).resolve())
-
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         raise ValueError('The path to the library is not set')
@@ -33,9 +22,9 @@ if __name__ == '__main__':
                 extension_dict[extension] = [name]
         library_name = extension_dict.get('.so', [])
         if len(library_name) == 0:
-            raise FileNotFoundError('There are no libraries with the .so extension in the directory')
+            raise FileNotFoundError('There are no libraries with the *.so extension in the directory')
         elif len(library_name) > 1:
-            raise ValueError('Libraries with an extension .so more than one')
+            raise ValueError('Libraries with an extension *.so more than one')
 
 
         for name in files:
@@ -43,8 +32,12 @@ if __name__ == '__main__':
                 parser_tree.parser_tree_from_file(os.path.join(root, name), ['-nostdinc','-std=c++17'])
 
 
+    if path_to_project[-1] != '/':
+        path_to_project += '/'
 
-    #parser_tree.serialize_data_to_binary_file("/home/maria/diploma/parser/tests/test") 
-    parser_tree.serialize_data_to_binary_file("/home/maria/diploma/parser/tests/test") 
+    print(path_to_project + library_name[0])
+    print(path_to_project)
+    print(library_name[0][:-3]+'_proto')
+    parser_tree.serialize_data_to_binary_file(path_to_project + library_name[0][:-3]+'_proto') 
     del parser_tree
                 
